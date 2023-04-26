4d0bd2c178d48c5d6d4105247ab9502c70dea275
ISURU <isuruumanga123@gmail.com>
2023-04-26 21:41:20 +0530

    employee view

--- src/Components/EmployeeView.js
+++ src/Components/EmployeeView.js
@@ -1,54 +1,73 @@
 import React, { Component } from "react";
+import ellipse1 from "../images/ellipse1.svg";
+import polygon from "../images/Polygon.svg";
+import ellipse2 from "../images/ellipse2.svg";
+import ellipse3 from "../images/ellipse3.svg";
+import logo from "../images/logo.svg";
+import profileimage from "../images/profile-image2.svg";
 
 import "../Styles/EmployeeView.css";
-import axios from 'axios';
-import EmployeeRow from './EmployeeRow';
+import axios from "axios";
+import EmployeeRow from "./EmployeeRow";
 
 export default class EmployeeView extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { employee: [], search: "" };
+		this.state.Station = this.props.match.params.id;
 
+		this.onChangeSearch = this.onChangeSearch.bind(this);
+	}
 
-	
-    constructor(props) {
-        super(props);
-        this.state = {employee : [], search:''};
-        this.state.Station = this.props.match.params.id;
+	onChangeSearch(e) {
+		this.setState({
+			search: e.target.value,
+		});
+	}
 
-        this.onChangeSearch = this.onChangeSearch.bind(this);
-    }
+	componentDidMount() {
+		// alert('email is ' +this.props.match.params.id);
+		axios
+			.get("http://localhost:4000/emplooyee/getall/")
+			.then((response) => {
+				// alert('Pass una')
+				// alert('Data Tika :'+response.data)
+				this.setState({ employee: response.data });
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
 
-    onChangeSearch(e){
-        this.setState( {
-           search: e.target.value
-        });
-
-    }
-
-    componentDidMount() {
-        // alert('email is ' +this.props.match.params.id);
-        axios.get('http://localhost:4000/emplooyee/getall/')
-            .then(response => {
-                // alert('Pass una')
-                // alert('Data Tika :'+response.data)
-                this.setState({employee : response.data});
-
-            })
-            .catch(function (error){
-                console.log(error);
-            })
-    }
-
-    tabRow(){
-        return this.state.employee.map(function (object, i){
-            return <EmployeeRow obj = {object} key = {i}/>;
-        });
-        // return <OrderTableRow obj={this.state.orders}/>
-    }
-
+	tabRow() {
+		return this.state.employee.map(function (object, i) {
+			return <EmployeeRow obj={object} key={i} />;
+		});
+		// return <OrderTableRow obj={this.state.orders}/>
+	}
 
 	render() {
 		return (
 			<div className='EmployeeView'>
+				<nav>
+					<img src={logo} alt='' />
+					<ul>
+						<li>Home</li>
+						<li>Offers</li>
+						<li>About Us</li>
+						<li>Contact Us</li>
+					</ul>
+					<div className='profile'>
+						<img src={profileimage} alt='' />
+					</div>
+				</nav>
+				<img src={ellipse1} alt='' className='ellipse1' />
 				<h2 className='main-header'>Employee</h2>
+				<form action=''>
+					<input type='text' placeholder='search...' />
+					<button type='submit'>Search</button>
+				</form>
+				<p className="add"><a href=""> + </a></p>
 
 				<table class='table table-striped'>
 					{" "}
@@ -61,7 +80,7 @@ export default class EmployeeView extends Component {
 						<th>Status</th>
 						<th>Action</th>
 					</thead>
-					{/* <tr className='users'>
+					<tr className='users'>
 						<td className='user'>
 							<img
 								src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
@@ -75,12 +94,34 @@ export default class EmployeeView extends Component {
 						</td>
 						<td>Event Planner</td>
 						<td>Active</td>
-						<td className="actions">
+						<td className='actions'>
 							<i class='fa-solid fa-pen'></i>
 							<i class='fa-solid fa-exclamation'></i>
 						</td>
-					</tr> */}
+					</tr>
+					<tr className='users'>
+						<td className='user'>
+							<img
+								src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+								alt=''
+							/>
+						</td>
+						<td>Name</td>
+						<td>20</td>
+						<td>
+							<a href=''>email123@gmail.com</a>{" "}
+						</td>
+						<td>Event Planner</td>
+						<td>Active</td>
+						<td className='actions'>
+							<i class='fa-solid fa-pen'></i>
+							<i class='fa-solid fa-exclamation'></i>
+						</td>
+					</tr>
 				</table>
+				<img src={polygon} alt='' className='polygon1' />
+				<img src={ellipse2} alt='' className='ellipse2' />
+				<img src={ellipse3} alt='' className='ellipse3' />
 			</div>
 		);
 	}
